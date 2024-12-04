f136ea6de9db85c2fefb9cf4c255a53c65e8ac18
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-12-04 13:24:09 -0600

    Update 9ConvertirCreatedDataTime.py
    mejora de codigo, no se estaba guardado jeje

--- Limpieza/9ConvertirCreatedDataTime.py
+++ Limpieza/9ConvertirCreatedDataTime.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-#convertir la columna createdAt a datetime
-df['createdAt'] = pd.to_datetime(df["createdAt"])
-
-#verificar cambios
-print("df.types")
+df = pd.read_csv("/dbfs/mnt/PagWeb/Extract/separados_web_limpio.csv")
+# Convertir la columna createdAt a datetime
+df['createdAt'] = pd.to_datetime(df['createdAt'])
+df.to_csv("/dbfs/mnt/PagWeb/Extract/separados_web_datetime.csv", index=False)
+display(df)
