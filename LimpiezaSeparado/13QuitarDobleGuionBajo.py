84108cbfd1ca764b2a7cf4d5208dbf34b4d6c5de
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2025-01-16 16:31:16 -0600

    Create 13QuitarDobleGuionBajo.py
    quita guion bajo doble y solo deja un guion bajo

---
+++ LimpiezaSeparado/13QuitarDobleGuionBajo.py
@@ -0,0 +1,15 @@
+# Leer el archivo CSV
+df = pd.read_csv("/dbfs/mnt/PagWeb/Extract/Separados/separados_web_guionesBajos.csv")
+
+# Lista de columnas a modificar
+columnas_a_modificar = ['asesor_nombre', 'cliente_nombreCompleto', 'lote_manzana_etapa_fraccionamiento_nombre'] #se agregan las columnas necesarias
+
+# Reemplazar espacios por guiones bajos en las columnas seleccionadas
+for columna in columnas_a_modificar:
+    df[columna] = df[columna].apply(lambda x: x.replace('__', '_') if isinstance(x, str) else x)
+
+# Guardar el DataFrame modificado en un nuevo archivo CSV
+df.to_csv("/dbfs/mnt/PagWeb/Extract/Separados/separados_web_sinGuionesDobles.csv", index=False)
+
+# Mostrar el DataFrame modificado
+display(df)
