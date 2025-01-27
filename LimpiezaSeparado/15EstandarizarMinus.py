1c404ef14557c994a5ab22a7f213fd1f1652603c
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2025-01-27 14:25:06 -0600

    update
    olvide el metodo

--- LimpiezaSeparado/15EstandarizarMinus.py
+++ LimpiezaSeparado/15EstandarizarMinus.py
@@ -5,7 +5,14 @@ from unidecode import unidecode
 df = pd.read_csv("/dbfs/mnt/PagWeb/Extract/Separados/separados_web_AllClean_lowercase.csv")
 
 #lista de columnas en las que deseas eliminar acentos
-columns_to_modify = ['cliente_primerNombre', 'cliente_segundoNombre', 'cliente_primerApellido', 'cliente_segundoApellido', 'cliente_nombreCompleto', 'lote_sku', 'lote_manzana_etapa_fraccionamiento_nombre', 'lote_fachada_modelo_nombre', 'estatus_nombre', 'asesor_nombre']
+columns_to_modify = [
+    'cliente_primerNombre', 'cliente_segundoNombre', 'cliente_primerApellido', 'cliente_segundoApellido', 'cliente_nombreCompleto', 'lote_sku',
+    'lote_manzana_etapa_fraccionamiento_nombre', 'lote_fachada_modelo_nombre', 'estatus_nombre', 'asesor_nombre'
+    ]
+
+#aplicar unidecode a cada columna de la lista
+for column in columns_to_modify:
+    df[column] = df[column].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
 
 #quitar acentos a registros de las columnas seleccionadas
 output_path = "/dbfs/mnt/PagWeb/Extract/Separados/separados_web_AllClean.csv"
