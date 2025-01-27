7675459e33c274e9c04a2c0819f7066b08863a1d
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2025-01-27 15:58:00 -0600

    Create 13QuitarAcentos.py
    quitta los acentos de los registros en las columnas indicadas

---
+++ LimpiezaVendido/13QuitarAcentos.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from unidecode import unidecode
+
+#cargar el dataframe
+df = pd.read_csv("/dbfs/mnt/PagWeb/Extract/Vendidos/vendidos_web_AllClean.csv")
+
+#lista de columnas para quitar acentos
+columns_to_modify = [
+    'cliente_primerNombre', 'cliente_segundoNombre', 'cliente_primerApellido', 'cliente_segundoApellido', 'cliente_nombreCompleto', 'lote_sku',
+    'lote_manzana_etapa_fraccionamiento_nombre', 'lote_fachada_modelo_nombre', 'estatus_nombre', 'asesor_nombre'
+    ]
+
+#aplicar unidecode a cada columna de la lista
+for column in columns_to_modify:
+    df[column] = df[column].apply(lambda x: unidecode(x) if isinstance(x, str) else x)
+
+#quitar acentos de los registros
+output_path = "/dbfs/mnt/PagWeb/Extract/Vendidos/vendidos_web_Acentos.csv"
+df.to_csv(output_path, index=False)
+
+#verificar
+print(f"Archivo guardado en: {output_path}")
+display(df.head())
